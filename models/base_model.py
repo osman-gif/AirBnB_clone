80b51f442769054412a20124cc3a2c502eb4db1f
osman-gif <osmanngong@gmail.com>
2024-05-13 16:51:49 +0200

    Added models file

--- models/base_model.py
+++ models/base_model.py
@@ -6,6 +6,7 @@ file_storage = __import__('models')
 # from models import storage
 """ This is the AirBnbClone console project """
 
+
 class BaseModel():
 
     def __init__(self, *args, **kwargs):
@@ -16,31 +17,36 @@ class BaseModel():
             file_storage.storage.new(self)
 
         else:
-            for k, value in kwargs.items():
+            for k, v in kwargs.items():
                 if k != '__class__':
                     if k in ['created_at', 'updated_at']:
-                        setattr(self, k, datetime.datetime.fromisoformat(value))
+                        setattr(self, k, datetime.datetime.fromisoformat(v))
                     else:
-                        setattr(self, k, value) 
+                        setattr(self, k, v)
 
     def __str__(self):
         new_dict = {}
-        for key, value in  self.__dict__.items():
+        for key, value in self.__dict__.items():
             new_dict[key] = value
-        for key, value in {'id': self.id, 'created_at': self.created_at, 'updated_at': self.updated_at}.items():
+        class_dict = {'id': self.id, 'created_at': self.created_at,
+                      'updated_at': self.updated_at}.items()
+        for key, value in class_dict:
             new_dict[key] = value
         new_dict['__class__'] = self.__class__.__name__
-        return '[{}] ({}) {}'.format(self.__class__.__name__, self.id, new_dict)
-    
+        return '[{}] ({}) {}'.format(self.__class__.__name__,
+                                     self.id, new_dict)
+
     def save(self):
         self.updated_at = datetime.datetime.now()
         file_storage.storage.save()
 
     def to_dict(self):
         new_dict = {}
-        for key, value in  self.__dict__.items():
+        class_dict = {'id': self.id, 'created_at': self.created_at,
+                      'updated_at': self.updated_at}.items()
+        for key, value in self.__dict__.items():
             new_dict[key] = value
-        for key, value in {'id': self.id, 'created_at': str(self.created_at), 'updated_at': str(self.updated_at)}.items():
+        for key, value in class_dict:
             new_dict[key] = value
         new_dict['__class__'] = self.__class__.__name__
-        return new_dict          
+        return new_dict
--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -2,12 +2,13 @@
 
 import json
 import os
-from models.base_model import BaseModel
+from base_model import BaseModel
+
 
 class FileStorage():
     """
-    This models the Filestorage storage type, it has functions for serializing python
-    dictionary to json, and deserializing json back to python dictionary
+    This models the Filestorage storage type, it has functions for serializing
+    python dictionary to json, and deserializing json back to python dictionary
     """
 
     __file_path = 'file.json'
@@ -19,26 +20,26 @@ class FileStorage():
         return the file path
         """
         return self.__file_path
-    
+
     @property
     def get_objects(self):
         """
         Returns the __objects variable
         """
         return self.__objects
-    
+
     def all(self):
         """
         returns the __objects variable, all objects
         """
         return self.__objects
-    
+
     def new(self, obj):
         """
         adds the newly created object 'obj' to __.objects dictionary
         """
         self.__objects['{}.{}'.format(obj.__class__.__name__, obj.id)] = obj
-    
+
     def save(self):
         """
         Convert the dictionary of objects '__objects' into json format and
@@ -49,7 +50,7 @@ class FileStorage():
             new_dict[k] = v.to_dict()
         with open(self.__file_path, 'w', encoding='utf-8') as file:
             json.dump(new_dict, file)
-    
+
     def reload(self):
         """
         Reads the contents of the json file, convert them from json
